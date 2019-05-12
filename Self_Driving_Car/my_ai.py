1f90d0e2057486f7dac014ab776201103b09e945
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2019-05-12 09:01:14 -0500

    Fixed load of neural network. Added output file as an object attribute

--- Self_Driving_Car/my_ai.py
+++ Self_Driving_Car/my_ai.py
@@ -53,6 +53,7 @@ class DeepQNetwork(object):
         self.last_state = torch.Tensor(input_size).unsqueeze(0)
         self.last_reward = 0
         self.last_action = 0
+        self.output_file = '../output/last_brain.pth'
 
     def select_action(self, state):
         temperature = 7
@@ -65,9 +66,9 @@ class DeepQNetwork(object):
 
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         outputs = self.neural_network(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        #outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        # outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
         next_outputs = self.neural_network(batch_next_state).detach().max(1)[0]
-        target = self.gamma *  next_outputs + batch_reward
+        target = self.gamma * next_outputs + batch_reward
         td_loss = F.smooth_l1_loss(outputs, target)
         self.optimizer.zero_grad()
         td_loss.backward(retain_variables=True)
@@ -81,7 +82,7 @@ class DeepQNetwork(object):
         action = self.select_action(new_state)
 
         if len(self.memory.memory) > 100:
-            batch_state, batch_next_state,  batch_action, batch_reward = self.memory.sample(100)
+            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
             self.learn(batch_state, batch_next_state, batch_reward, batch_action)
 
         self.last_action = action
@@ -94,17 +95,18 @@ class DeepQNetwork(object):
         return action
 
     def score(self):
-        return sum(self.reward_window)/(len(self.reward_window)+1.)
+        return sum(self.reward_window) / (len(self.reward_window) + 1.)
 
     def save(self):
         torch.save({'state_dict': self.neural_network.state_dict(),
-                    'optimizer': self.optimizer.state_dict()}, 'last_brain.pth')
+                    'optimizer': self.optimizer.state_dict()},
+                   self.output_file)
 
     def load(self):
-        if os.path.isfile('last_brain.pth'):
+        if os.path.isfile(self.output_file):
             print("=> loading checkpoint... ")
-            checkpoint = torch.load('last_brain.pth')
-            self.model.load_state_dict(checkpoint['state_dict'])
+            checkpoint = torch.load(self.output_file)
+            self.neural_network.load_state_dict(checkpoint['state_dict'])
             self.optimizer.load_state_dict(checkpoint['optimizer'])
             print("done !")
         else:
